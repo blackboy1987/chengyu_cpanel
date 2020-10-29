331df8645b77507c9e687a75ec9b2f4e68c58934
1169794338@qq.com <1169794338@qq.com>
2020-10-29 20:59:12 +0800

    init

--- src/main/java/com/bootx/config/WebConfig.java
+++ src/main/java/com/bootx/config/WebConfig.java
@@ -1,7 +1,7 @@
 package com.bootx.config;
 
 import com.bootx.interceptor.CorsInterceptor;
-import com.bootx.miniprogram.interceptor.AppValidateInterceptor;
+import com.bootx.interceptor.ValidateInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,18 +14,17 @@ public class WebConfig implements WebMvcConfigurer {
     public CorsInterceptor corsInterceptor() {
         return new CorsInterceptor();
     }
-
     @Bean
-    public AppValidateInterceptor appValidateInterceptor () {
-        return new AppValidateInterceptor();
+    public ValidateInterceptor validateInterceptor() {
+        return new ValidateInterceptor();
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(corsInterceptor())
                 .addPathPatterns("/**");
-       /* registry.addInterceptor(appValidateInterceptor())
-                .addPathPatterns("/**");*/
+       /*registry.addInterceptor(validateInterceptor())
+                .addPathPatterns("/**"); */
 
     }
 
